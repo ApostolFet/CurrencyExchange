1cb9fc37586f4e31050e233a03324e4b29cc99d4
Apostol Fet <maksim170901@gmail.com>
2024-10-13 17:30:46 +0300

    feat: create db schema migrations

---
+++ src/currency_exchange/infrastructure/database/migrations/create_tables_currency_exchange.py
@@ -0,0 +1,59 @@
+from sqlite3 import Connection, connect
+
+
+def up() -> None:
+    db_path = "currency_exchange.db"
+    connection = connect(db_path)
+    try:
+        upgrade(connection)
+    finally:
+        connection.close()
+
+
+def down() -> None:
+    db_path = "currency_exchange.db"
+    connection = connect(db_path)
+    try:
+        downgrade(connection)
+    finally:
+        connection.close()
+
+
+def upgrade(connection: Connection) -> None:
+    with connection as conn:
+        conn.executescript(
+            """
+        CREATE TABLE IF NOT EXISTS currencies (
+            id BLOB(16) PRIMARY KEY,
+            code VARCHAR(3) UNIQUE,
+            sign VARCHAR(255),
+            name VARCHAR(255)
+        );
+
+        CREATE TABLE IF NOT EXISTS exchange_rates (
+            id BLOB(16) PRIMARY KEY,
+            base_currency BLOB(16) REFERENCES currencies(id) ON DELETE CASCADE,
+            target_currency BLOB(16) REFERENCES currencies(id) ON DELETE CASCADE,
+            rate DECIMAL(16, 6)
+        );
+
+        CREATE INDEX IF NOT EXISTS exchange_rates_base_currency ON
+        exchange_rates(base_currency);
+
+        CREATE INDEX IF NOT EXISTS exchange_rates_target_currency
+        ON exchange_rates(target_currency);
+        """,
+        )
+
+
+def downgrade(connection: Connection) -> None:
+    with connection as conn:
+        conn.executescript(
+            """
+        DROP INDEX IF EXISTS exchange_rates_target_currency;
+        DROP INDEX IF EXISTS exchange_rates_base_currency;
+
+        DROP TABLE IF EXISTS exchange_rates;
+        DROP TABLE IF EXISTS currencies;
+        """,
+        )
