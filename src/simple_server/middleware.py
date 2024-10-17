8c70c3f0c30efde3648e1517e10557ed23ee360e
Apostol Fet <maksim170901@gmail.com>
2024-10-17 20:10:57 +0300

    feat: add cors middleware

--- src/simple_server/middleware.py
+++ src/simple_server/middleware.py
@@ -1,7 +1,33 @@
-from typing import Protocol
+from typing import Protocol, override
 
 from simple_server.types import Handler, Request, Response
 
 
 class Middleware(Protocol):
     def __call__(self, handler: Handler, request: Request) -> Response: ...
+
+
+class CORSMiddleware(Middleware):
+    def __init__(
+        self,
+        *,
+        allow_origins: list[str],
+        allow_methods: list[str],
+        allow_headers: list[str],
+        allow_credentials: bool,
+    ):
+        self._allow_origins = ", ".join(allow_origins)
+        self._allow_methods = ", ".join(allow_methods)
+        self._allow_headers = ", ".join(allow_headers)
+        self._allow_credentials = allow_credentials
+
+    @override
+    def __call__(self, handler: Handler, request: Request) -> Response:
+        response = handler(request)
+        response.headers["Access-Control-Allow-Origin"] = self._allow_origins
+        response.headers["Access-Control-Allow-Methods"] = self._allow_methods
+        response.headers["Access-Control-Allow-Headers"] = self._allow_headers
+        if self._allow_credentials:
+            response.headers["Access-Control-Allow-Credentials"] = "true"
+
+        return response
